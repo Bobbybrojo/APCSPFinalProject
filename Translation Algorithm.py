085c910b9f9cdce4410b03c499161e38d8c541ca
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2019-01-23 00:53:06 -0800

    Add files via upload

--- Translation Algorithm.py
+++ Translation Algorithm.py
@@ -1,25 +1,46 @@
 
+# alphabet list for functions to grab from
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            # second set for offsets up to 26 from z key
+            ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# define global variables and prompt user
+global key
+global phrase
+key = input("What is your offset key for encryption/decryption? (A Number 0-26): ")
+option = input("Would you like to encrypt or decrypt?(Enter E/D): ")
+phrase = input("Enter the phrase you would like to be encrypted/decrypted: ")
 
 
-def translate(string):
+# encrypt
+def encrypt(string):
     current_char = 0
-    char = " "
+    print("Your encrypted code for " + string + " is: ")
     while current_char < len(string):
-        alphabet_num = alphabet.index(char.lower())
-        print(alphabet[alphabet_num + 1])
-        current_char += 1
         char = string[current_char]
 
+        print(alphabet[alphabet.index(char.lower()) + int(key)])
+        current_char += 1
 
-def translate_tester(string):
+
+# decrypt
+def decrypt(string):
     current_char = 0
+    print("Your decrypted code for " + string + " is: ")
     while current_char < len(string):
         char = string[current_char]
-        print(string[current_char])
-        print(alphabet.index(char.lower()))
-        print(alphabet[alphabet.index(char.lower()) + 2])
+
+        print(alphabet[alphabet.index(char.lower()) - int(key)])
         current_char += 1
-translate_tester("Bruh momentum")
+
+
+# decrypt or encrypt based on user input
+if option.lower() == "e":
+    encrypt(phrase)
+elif option.lower() == "d":
+    decrypt(phrase)
+else:
+    print("You did not properly enter either 'e' or 'd' to encrypt or decrypt")
 
